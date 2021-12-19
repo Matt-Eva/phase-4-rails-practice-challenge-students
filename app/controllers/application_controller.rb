465fc7b0be6105fe1bfcae94da1e2c064b65c238
Matt Eva <matteweva@gmail.com>
2021-12-19 13:58:42 -0800

    commit tree ba5db330060fc45dbc9cd60d0418be93b92f4caa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :not_found
+rescue_from ActiveRecord::RecordInvalid, with: :unprocessable
+
+private
+
+def not_found(exception)
+    render json: {error: "#{exception.model} not found"}, status: :not_found
+end
+
+def unprocessable(invalid)
+    render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+end
+
 end
